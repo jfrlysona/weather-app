d45050191926dcca863b40913be6c14e38543ec4
jfrlysona <mi7xxzdth@code.edu.az>
2024-01-16 04:26:54 +0400

    updated

--- Desktop/dazzling fe/src/components/Cards.jsx
+++ Desktop/dazzling fe/src/components/Cards.jsx
@@ -4,8 +4,10 @@ import { useState } from "react";
 function Cards() {
   const [cards, setCards] = useState([]);
   useEffect(() => {
-    fetch("")
-  }, []);
+    fetch("http://localhost:3000/posts/");
+  }, [])
+    .then((res) => res.json())
+    .then((data) => setCards(data));
 
   return <div className="cards">Cards</div>;
 }
